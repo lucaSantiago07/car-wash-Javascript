6fb679c88687b39e1d6c551a57cb9d0073d79c98
Lucas Santiago <izaias.santiago.07@gmail.com>
2023-07-11 23:50:34 -0300

    Criação do menu responsivo
    O objetivo é fazer com que o menu assim como todo o site seja utilizável tanto na web quanto em mobile

---
+++ index.js
@@ -0,0 +1,19 @@
+function openMenuMobile () {
+   
+    let menuMobile = document.getElementById('navMobile')
+    let classMenuMobile = menuMobile.getAttribute('class') 
+    
+    let classMenuMobileOpen = classMenuMobile + ' open'
+    
+    if (classMenuMobile.match('open')) {
+        classMenuMobile = classMenuMobileOpen.substring(0,49)
+        menuMobile.setAttribute('class',classMenuMobile)
+    }
+    else {
+        menuMobile.setAttribute('class',classMenuMobileOpen)
+    }
+
+    
+}
+
+document.getElementById('btnMobile').addEventListener('click', openMenuMobile)
