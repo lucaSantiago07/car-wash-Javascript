ec8237ddbe9a0388fc320e0bbcfde91ac2f3ab63
Lucas Santiago <izaias.santiago.07@gmail.com>
2023-07-16 10:07:49 -0300

    Iniciando Modularização das funções - Begin functions modularization

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Front-end Function Mobile
 function openMenuMobile () {
     let menuMobile = document.getElementById('navMobile')
     let classMenuMobile = menuMobile.getAttribute('class') 
@@ -12,8 +12,33 @@ function openMenuMobile () {
     else {
         menuMobile.setAttribute('class',classMenuMobileOpen)
     }
-    
 }
 
 document.getElementById('btnMobile').addEventListener('click', openMenuMobile)
 
+
+//Front-end Function
+function switchSymb (button, text, sizeFont) {
+    let scrollDownArrow = document.getElementById(button)
+    scrollDownArrow.style.paddingLeft = '2rem'
+       scrollDownArrow.innerHTML = text
+       scrollDownArrow.style.fontSize = sizeFont
+       
+}
+
+document.getElementById('scrollDownArrow').addEventListener('mouseenter',function() {
+    switchSymb ('scrollDownArrow','Scroll Down', '1.2rem')
+})
+
+
+//Front-end Function
+function returnSymb (button, fontSize, text) {
+    let scrollDownArrow = document.getElementById(button)
+    scrollDownArrow.style.fontSize = fontSize    
+    scrollDownArrow.innerHTML = text
+    
+}
+ 
+document.getElementById("scrollDownArrow").addEventListener('mouseleave', function () {
+    returnSymb ('scrollDownArrow', '2.2rem', '&#10096&#10096') 
+})
